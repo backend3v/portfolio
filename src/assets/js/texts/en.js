48b6e78535fa2098daf7cd31bc57fd9f1ed32eac
backend3v <163341809+backend3v@users.noreply.github.com>
2024-12-05 17:01:35 -0500

    Update en.js

--- src/assets/js/texts/en.js
+++ src/assets/js/texts/en.js
@@ -3,11 +3,11 @@ const texts = {
         title: "About Me",
         contact: "Contact",
         social: "Social Networks",
-        github: "GitHub Repositories",
+        github: "Github Repositories",
         colab: "Colab Notebooks",
         position: "Software Developer",
         name: "Edward Camilo Gonzalez",
-        about: "With over 7 years of experience in various startups including Stradata, Symplifica (Top 5 most innovative companies in Latin America by FAST COMPANY 2023), Mangosoft, and Tulpep. I have developed software for various industries, acquiring the experience and ability to work autonomously.",
+        about: "With over 7 years of experience in various startups including Stradata, Symplifica (Top 5 most innovative companies in Latin America by FAST COMPANY 2023), Mangosoft, and Tulpep. Developing software for various industries, I have acquired the experience and the ability to work autonomously.",
         email1: "edward1577@gmail.com",
         email2: "backender.dev@gmail.com",
         linkedin: "LinkedIn",
@@ -15,30 +15,35 @@ const texts = {
         backendev: "Backendev",
         colab1: "Colab 1",
         colab2: "Colab 2",
-        colab3: "Colab 3tt"
+        colab3: "Colab 3tt",
     },
     skills: {
         title: "Skills",
-        skills:"Skills",
-        skillsTechs: "Tecnologíes",
+        skills: "Skills",
+        skillsTechs: "Technologies",
         skillsTech: {
             python: "Web development, data analysis, artificial intelligence, and scientific computing",
             php: "Dynamic web development",
             ruby: "Web development",
             csharp: "Develop applications and games",
-            javascript: "Interactive web development. An essential part of web development alongside HTML and CSS",
+            javascript: "Interactive web development. It is an essential part of web development along with HTML and CSS",
             node: "JavaScript runtime environment, development of scalable network applications",
             sql: "Query language for relational databases",
-            mongo: "Document-oriented NoSQL database. Managing large amounts of data",
+            mongo: "NoSQL document-oriented database. Handles large amounts of data",
             linux: "Open-source Unix operating system. Known for its security and flexibility",
-            git: "Distributed version control system. Enables collaborative development",
-            docker: "Deployment of applications within lightweight and portable containers."
+            git: "Distributed version control system. Allows collaborative development",
+            docker: "Deployment of applications inside lightweight and portable containers.",
+            solidity: "Development of smart contracts. On the Blockchain network",
+            flutter: "Mobile application development",
+            aws: "Cloud application development",
+            terraform: "Cloud infrastructure development",
+            scikit: "Development of machine learning applications (AI)",
         },
         content: [
             {
-                name: "Data Analysis and Engineering",
+                name: "Data",
                 texts: [
-                    "Data Extraction, Treatment, and Loading",
+                    "Data Extraction, Processing, and Loading",
                     "Predictive and Descriptive Statistical Analysis of Data",
                     "Management of Relational and Non-Relational Databases",
                     "Development of interactive dashboards, Data Visualization",
@@ -51,118 +56,123 @@ const texts = {
                 texts: [
                     "Development of Web Applications and Microservices",
                     "Implementation of RESTful APIs",
-                    "Frontend Development using modern frameworks",
+                    "Frontend development using modern frameworks",
                     "Integration of SQL and NoSQL databases in web applications",
-                    "Optimization of web performance and SEO"
+                    "Web performance optimization and SEO",
                 ]
             },
             {
                 name: "Mobile Development",
                 texts: [
-                    "Development of Mobile Applications for iOS and Android",
-                    "Implementation of REST API in mobile applications",
+                    "Development of Mobile applications for iOS and Android",
+                    "Implementation of REST APIs in mobile applications",
                     "Integration of cloud services in mobile applications",
                     "Optimization of performance and user experience on mobile",
-                    "Deployment and publication in the App Store and Google Play"
+                    "Deployment and publishing in App Store and Google Play"
                 ]
             },
             {
                 name: "Desktop Applications",
                 texts: [
-                    "Development of Desktop Applications in a Linux environment",
+                    "Development of Desktop Applications in Linux environment",
                     "Integration and Automation of processes",
                     "Creation of graphical user interfaces (GUI) with Qt and Glade",
                     "Implementation of local databases in desktop applications",
                     "Development of system administration tools",
-                    "Integration of web services in desktop applications"
+                    "Integration of web services in desktop applications",
                 ]
             }
         ]
     },
     jobs: {
         title: "Jobs",
-        jobsT:"Jobs",
-        projectsT:"Projects",
+        jobsT: "Jobs",
+        projectsT: "Projects",
         jobs: [
             {
-                name: "Hospital San Ignacio",
-                position: "IT Support Technician",
-                description: [],
-            },
-            {
-                name: "Risk Possible",
-                position: "Data Engineer & Software Developer",
-                description: [
-                    "Software development, Insurance industry, Data ETL, web scraping, and SQL query optimization",
-                    "Python (Scrapy, Requests, Pandas), Azure CosmosDB, Bash, Linux, Docker"
-                ],
-            },
-            {
-                name: "Biznaga Media",
+                name: "Mango Soft",
                 position: "Software Developer",
                 description: [
-                    "Development of Web Applications, Rich media, and marketing campaign analysis services by Geolocation",
-                    "PHP, PowerShell, JavaScript, Bash, Git"
-                ],
-            },
-            {
-                name: "Tulpep - Symplifica",
-                position: "Software Developer",
-                description: [
-                    "Development of Symplifica's web application 2.0",
-                    "Python (Django), Ruby (On Rails), PHP (Symphony), Bash, Git, Docker, AWS, Linux"
+                    "Development and maintenance of software for the Health and Sports industry",
+                    "Python, AWS (CloudFormation, Lambda functions, APIs, SQS, EventBridge), Blockchain, Solidity, JavaScript (Angular), TypeScript, Docker, Linux, Git, Bash, Docker"
                 ]
             },
             {
                 name: "Stradata",
                 position: "Data Analyst and Software Developer",
                 description: [
-                    "Software development, Integration of ML models, and maintenance of Laravel application",
+                    "Software development, integration of ML models, and maintenance of Laravel application",
                     "Python (SciKit Learn), MongoDB, PHP (Laravel), JavaScript, Bash, Git, Docker, AWS, Linux"
                 ]
             },
             {
-                name: "Mango Soft",
+                name: "Tulpep - Symplifica",
                 position: "Software Developer",
                 description: [
-                    "Development and maintenance of software for the health and sports industry",
-                    "Python, AWS (CloudFormation, Lambda functions, APIs, SQS, EventBridge), Blockchain, Solidity, JavaScript (Angular), TypeScript, Docker, Linux, Git, Bash, Docker"
+                    "Development of Symplifica web 2.0 application",
+                    "Python (Django), Ruby (On Rails), PHP (Symphony), Bash, Git, Docker, AWS, Linux"
                 ]
+            },
+            {
+                name: "Biznaga Media",
+                position: "Software Developer",
+                description: [
+                    "Development of Web Applications, Richmedia, and geolocation-based marketing campaign analysis services",
+                    "PHP, PowerShell, JavaScript, Bash, Git"
+                ],
+            },
+            {
+                name: "Risk Posible",
+                position: "Data Engineer & Software Developer",
+                description: [
+                    "Software development, Insurance industry, ETL of data, web scraping, and optimization of SQL queries",
+                    "Python (Scrapy, Requests, Pandas), Azure CosmosDB, Bash, Linux, Docker"
+                ],
+            },
+            {
+                name: "Hospital San Ignacio",
+                position: "IT Support Technician",
+                description: [
+                    "Technical support and maintenance of computer equipment",
+                    "Automation of services and optimization of processes",
+                    "First-level support for biotechnical and medical equipment",
+                    "Support for Networks and Internet Services",
+                ],
             },
         ],
         projects: [
             {
                 name: "Notary Module (Mango Soft)",
                 description: [
-                    "Document notarization, Study on blockchain technologies, Development of smart contracts in Solidity; to test different types of contracts",
+                    "Document notarization, study on blockchain technologies, development of smart contracts in Solidity to test different types of contracts",
                     "Solidity, Python, DynamoDB, AWS Terraform, Docker"
                 ]
             },
             {
                 name: "Forms Module (Mango Soft)",
                 description: [
-                    "Creation of forms, Drag & Drop interface for Widgets, Automated deployment of this module for various clients. The project was successfully completed and is in production.",
+                    "Creation of forms, drag & drop interface for widgets, automated deployment of this module for various clients. The project was successfully completed and is in production",
                     "Python, Angular, TypeScript, AWS, Terraform, Docker, Git"
                 ]
             },
             {
                 name: "SAGRILAFT Application (Stradata)",
                 description: [
-                    "Creation of AI models to apply SAGRILAFT regulations and identify risks or non-compliance. Support for the Data Science department, Database management.",
+                    "Creation of AI models to apply SAGRILAFT regulations and identify risks or non-compliance. Support for the Data Science department, database administration.",
                     "Python, MongoDB, Azure"
                 ]
             },
             {
                 name: "Web 2.0 Application (Symplifica)",
                 description: [
-                    "Development of a Web 2.0 Application, considering the experience and state of the company at the time of project execution.",
+                    "Development of Web 2.0 Application, taking into account the company's experience and status at the time of project execution",
                     "PHP (Symphony), Python, Ruby on Rails, PostgreSQL, Docker, AWS"
                 ]
             },
             {
                 name: "Insurance Policy Analysis (Risk Possible)",
                 description: [
-                    "Development of scripts to scrape different sources to detect possible risks in insurance policies.",
+                    "Development of scripts for web scraping from different sources, with the aim of detecting possible risks in insurance policies",
                     "Python, Azure CosmosDB, PostgreSQL"
                 ],
             },
@@ -170,7 +180,24 @@ const texts = {
     },
     examples: {
         title: "Examples",
-        infoExample:["In this section, you can try my AI","Next, an example application is presented.","In the search bar, you can enter a word or a phrase related to a topic, and the AI will generate an infographic with descriptions and a comparative graph related to this topic","","","Examples:","","-Colombia","-Artificial Intelligence","Soccer","Nemésio Camacho El Campin Stadium","","","As you can see, they can be Places, Organizations, People, or any topic of your interest","Keep in mind that the phrase will be evaluated based on its total length; it is not recommended to make them too long, as the AI is not very efficient in these cases, and try to keep it coherent and as non-abstract as possible.","Enjoy!"]
+        infoExample: [
+            "In this section, you can try my AI",
+            "Below is an example application.",
+            "In the search bar, you can enter a word or phrase related to a topic, and the AI will generate an infographic with descriptions and a comparative graph related to this topic",
+            "",
+            "",
+            "Examples:",
+            "",
+            "-Colombia",
+            "-Artificial Intelligence",
+            "Soccer",
+            "Nemesio Camacho El Campin Stadium",
+            "",
+            "",
+            "As you can see, they can be Places, Organizations, People, or any topic of your interest.",
+            "Keep in mind that the phrase will be evaluated based on its total length; it is not recommended to make them too long, as the AI is not very efficient in these cases, and try to make it coherent and as less abstract as possible.",
+            "Enjoy it!"
+        ],
     }
 };
 export default texts;
